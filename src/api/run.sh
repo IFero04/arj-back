087df4d3a28e1cf651a58fa18cb6a7407f8fa448
Afonso Faria <afonsofariatech@gmail.com>
2024-09-24 14:19:54 +0100

    Update run script

--- src/api/run.sh
+++ src/api/run.sh
@@ -1,3 +1,7 @@
-echo $1
-
-python3 main.py
+if [[ $1 == "dev" ]]; then
+    python3 main.py
+elif [[ $1 == "sit" || $1 == "prod" ]]; then
+    python3 main.py
+else
+    echo "Need to follow format ./run.sh dev|sit|prod"
+fi
